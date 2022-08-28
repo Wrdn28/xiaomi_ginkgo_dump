9558efc7a493c78dabfc3ef796f6293168498b69
Akash Srivastava <akashniki@gmail.com>
2022-08-28 13:49:45 +0530

    ginkgo: manifest: Drop QTI wifi hals
    * These Are Addressed in CAF
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -395,9 +395,7 @@ PRODUCT_PACKAGES += \
     WifiOverlay \
     wpa_cli \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.0.vendor
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
