9ef12df70ee2341ee5cc87247ef2787d84c6916e
JaswantTeja <sunnyteja201@gmail.com>
2022-10-01 21:57:02 +0530

    ginkgo: Switch to common QTI Vibrator Component
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -304,6 +304,8 @@ PRODUCT_ODM_PROPERTIES += \
 TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     av \
+    vibrator \
+
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -403,10 +405,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
