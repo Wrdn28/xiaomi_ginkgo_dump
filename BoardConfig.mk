fb8dcaa807659526da1c46b4b62f50d6a740f273
0X01NOBREGAX <otavio.nobregadas@gmail.com>
2024-03-05 15:48:25 -0300

    ginkgo: Define old rmnet data services
    * As we don't use qti telephony component, these flags are set so
       that mobile data works normally, without them, it will not work.
    
    Change-Id: I079e22f8cd0ba9bffb0b224b4f6e5b6b3461fa9d
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,13 @@ TARGET_NEEDS_RAW10_BUFFER_FIX := true
 TARGET_USES_QTI_CAMERA_DEVICE := true
 TARGET_USES_MIUI_CAMERA := true
 
+# Data Services
+SOONG_CONFIG_NAMESPACES += rmnetctl
+SOONG_CONFIG_rmnetctl += \
+    old_rmnet_data
+
+SOONG_CONFIG_rmnetctl_old_rmnet_data := true
+
 # Display
 TARGET_SCREEN_DENSITY := 440
 
